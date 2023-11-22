c79549b0bd598c4f94deba49143bf7a650056265
rlarlduf20 <kky8274@naver.com>
2023-11-22 15:26:54 +0900

    Feat: stompjs를 사용해서 소켓 최초 설정

---
+++ src/app/album/loading.tsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <>로딩중입니다...</>;
+};
+
+export default Loading;
---
+++ src/app/album/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
+import SockJS from "sockjs-client";
+import { Client } from "@stomp/stompjs";
+import { useSession } from "next-auth/react";
+
+const AlbumCreatePage = () => {
+  const [msgList, setMsgList] = useState([]);
+  const [msg, setMsg] = useState("");
+  const client = useRef<any>({});
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    let accessToken = session?.jogakTokens.accessToken;
+
+    client.current = new Client({
+      brokerURL: `ws://${process.env.NEXT_PUBLIC_SOCKET_SERVER_URL}/album-ws`,
+      connectHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      onConnect: () => {
+        console.log("success");
+        client.current.subscribe("/topic/sub", (body: any) => {
+          const json_body = JSON.parse(body.body);
+          setMsgList((_chat_list): any => [..._chat_list, json_body]);
+        });
+      },
+    });
+    client.current.activate();
+
+    return () => {
+      console.log("연결 해제");
+      client.current.deactivate();
+    };
+  }, [session]);
+
+  const publish = (msg: string) => {
+    if (!client.current.connected) return;
+
+    client.current.publish({
+      destination: "/app/img",
+      body: msg,
+    });
+
+    setMsg("");
+  };
+  const sendMsg = () => {
+    publish(msg);
+  };
+
+  return (
+    <>
+      <input
+        value={msg}
+        placeholder="제목을 입력하세요"
+        onChange={(e) => setMsg(e.target.value)}
+      />
+      <button onClick={sendMsg}>전송</button>
+      <div>
+        {msgList.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AlbumCreatePage;
