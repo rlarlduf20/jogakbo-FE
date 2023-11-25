378f030fae3a29ee705bd69576a5ef775c54be16
rlarlduf20 <kky8274@naver.com>
2023-11-25 15:45:51 +0900

    Feat: 앨범 페이지 생성
    1. 페이지 이동버튼 생성
    2. 페이지마다 이미지 넣기
    3. 외부 이미지 파일 드래그 앤 드롭 시 css 변화 및 파일 정보 가져오기

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -1,68 +1,178 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
-import SockJS from "sockjs-client";
-import { Client } from "@stomp/stompjs";
-import { useSession } from "next-auth/react";
+
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const albumMockDataList = [
+  {
+    page: 1,
+    imgList: [
+      { src: "/images/lion.png", locationX: 30, locationY: 50 },
+      { src: "/images/kakao_login_btn.png", locationX: 90, locationY: 80 },
+    ],
+  },
+  {
+    page: 2,
+    imgList: [{ src: "/images/rabbit.png", locationX: 100, locationY: 70 }],
+  },
+  {
+    page: 3,
+    imgList: [{ src: "/images/smileBall.png", locationX: 10, locationY: 10 }],
+  },
+  { page: 4, imgList: [{ src: "", locationX: 40, locationY: 40 }] },
+];
 
 const AlbumCreatePage = () => {
-  const [msgList, setMsgList] = useState([]);
-  const [msg, setMsg] = useState("");
-  const client = useRef<any>({});
-  const { data: session } = useSession();
+  const canvasLeftRef = useRef<any>();
+  const canvasRightRef = useRef<any>();
+  const [page, setPage] = useState(1);
+  const [isDrag, setIsDrag] = useState(false);
+  const handleDrop = useCallback((event: any) => {
+    event.preventDefault();
+    setIsDrag(false);
+    const files = event.dataTransfer.files;
+    console.log(files);
+  }, []);
+  const handleDragOver = useCallback((event: any) => {
+    event.preventDefault();
+    setIsDrag(true);
+  }, []);
 
   useEffect(() => {
-    let accessToken = session?.jogakTokens.accessToken;
+    const renderCanvas = (canvasRef: any, currentPage: number) => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      console.log(currentPage);
+      const currentPageData = albumMockDataList.find(
+        (data) => data.page === currentPage
+      );
 
-    client.current = new Client({
-      brokerURL: `ws://${process.env.NEXT_PUBLIC_SOCKET_SERVER_URL}/album-ws`,
-      connectHeaders: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      onConnect: () => {
-        console.log("success");
-        client.current.subscribe("/topic/sub", (body: any) => {
-          const json_body = JSON.parse(body.body);
-          setMsgList((_chat_list): any => [..._chat_list, json_body]);
+      if (currentPageData) {
+        currentPageData.imgList.forEach(({ src, locationX, locationY }) => {
+          if (src) {
+            const image = new Image();
+            image.src = src;
+            image.onload = () => {
+              ctx.drawImage(image, locationX, locationY);
+            };
+          }
         });
-      },
-    });
-    client.current.activate();
-
-    return () => {
-      console.log("연결 해제");
-      client.current.deactivate();
+      }
     };
-  }, [session]);
 
-  const publish = (msg: string) => {
-    if (!client.current.connected) return;
+    renderCanvas(canvasLeftRef, page);
+    renderCanvas(canvasRightRef, page + 1);
+  }, [page]);
 
-    client.current.publish({
-      destination: "/app/img",
-      body: msg,
-    });
-
-    setMsg("");
+  const onClickPrev = () => {
+    setPage((prev) => prev - 1);
   };
-  const sendMsg = () => {
-    publish(msg);
+  const onClickNext = () => {
+    setPage((prev) => prev + 1);
   };
 
   return (
-    <>
-      <input
-        value={msg}
-        placeholder="제목을 입력하세요"
-        onChange={(e) => setMsg(e.target.value)}
-      />
-      <button onClick={sendMsg}>전송</button>
-      <div>
-        {msgList.map((item, index) => (
-          <p key={index}>{item}</p>
-        ))}
+    <section>
+      <div className="w-[1200px] flex">
+        <button
+          className={`${page !== 1 && "hover:cursor-pointer"}`}
+          disabled={page === 1}
+          onClick={onClickPrev}
+        >
+          이전
+        </button>
+        <input className="border-2 block m-auto" />
+        {page === albumMockDataList.length - 1 ? (
+          <button className="hover:cursor-pointer" onClick={onClickNext}>
+            생성
+          </button>
+        ) : (
+          <button className="hover:cursor-pointer" onClick={onClickNext}>
+            다음
+          </button>
+        )}
       </div>
-    </>
+
+      <div className="flex gap-[40px]">
+        <canvas
+          ref={canvasLeftRef}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onDragLeave={() => {
+            setIsDrag(false);
+          }}
+          className={`${isDrag ? "border-4" : "border-2"} w-[580px] h-[600px]`}
+        ></canvas>
+        <canvas
+          ref={canvasRightRef}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onDragLeave={() => {
+            setIsDrag(false);
+          }}
+          className={`${isDrag ? "border-4" : "border-2"} w-[580px] h-[600px]`}
+        ></canvas>
+      </div>
+    </section>
   );
 };
 
 export default AlbumCreatePage;
+
+// import { useState, useEffect, useRef } from "react";
+// import SockJS from "sockjs-client";
+// import { Client } from "@stomp/stompjs";
+// import { useSession } from "next-auth/react";
+
+// const [msgList, setMsgList] = useState([]);
+// const [msg, setMsg] = useState("");
+// const client = useRef<any>({});
+// const { data: session } = useSession();
+// useEffect(() => {
+//   let accessToken = session?.jogakTokens.accessToken;
+//   client.current = new Client({
+//     brokerURL: `ws://${process.env.NEXT_PUBLIC_SOCKET_SERVER_URL}/album-ws`,
+//     connectHeaders: {
+//       Authorization: `Bearer ${accessToken}`,
+//     },
+//     onConnect: () => {
+//       console.log("연결 성공");
+//       client.current.subscribe("/topic/sub", (body: any) => {
+//         const json_body = JSON.parse(body.body);
+//         setMsgList((_msg_list): any => [..._msg_list, json_body]);
+//       });
+//     },
+//   });
+//   client.current.activate();
+//   return () => {
+//     console.log("연결 해제");
+//     client.current.deactivate();
+//   };
+// }, [session]);
+// const publish = (msg: string) => {
+//   if (!client.current.connected) return;
+
+//   client.current.publish({
+//     destination: "/app/img",
+//     body: msg,
+//   });
+//   setMsg("");
+// };
+// const sendMsg = () => {
+//   publish(msg);
+// };
+// return (
+//   <>
+//     <input
+//       value={msg}
+//       placeholder="제목을 입력하세요"
+//       onChange={(e) => setMsg(e.target.value)}
+//     />
+//     <button onClick={sendMsg}>전송</button>
+//     <div>
+//       {msgList.map((item, index) => (
+//         <p key={index}>{item}</p>
+//       ))}
+//     </div>
+//   </>
+// );
