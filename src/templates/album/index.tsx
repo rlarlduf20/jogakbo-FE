43c5647da6c39c6fca73dbc4d8733b893373eb5d
rlarlduf20 <kky8274@naver.com>
2023-11-27 15:12:37 +0900

    Refactor: 캔버스 렌더함수 파일 분리

--- src/templates/album/index.tsx
+++ src/templates/album/index.tsx
@@ -2,12 +2,14 @@
 
 import { useCallback, useEffect, useRef, useState } from "react";
 import { albumInfo, albumMockDataList } from "./assets/mockData";
+import { renderCanvas } from "./lib/utils";
 
 const AlbumSection = () => {
   const canvasLeftRef = useRef<any>();
   const canvasRightRef = useRef<any>();
   const [page, setPage] = useState(1);
   const [isDrag, setIsDrag] = useState(false);
+
   const handleDrop = useCallback((event: any) => {
     event.preventDefault();
     setIsDrag(false);
@@ -20,30 +22,8 @@ const AlbumSection = () => {
   }, []);
 
   useEffect(() => {
-    const renderCanvas = (canvasRef: any, currentPage: number) => {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      console.log(currentPage);
-      const currentPageData = albumMockDataList.find(
-        (data) => data.page === currentPage
-      );
-
-      if (currentPageData) {
-        currentPageData.imgList.forEach(({ src, locationX, locationY }) => {
-          if (src) {
-            const image = new Image();
-            image.src = src;
-            image.onload = () => {
-              ctx.drawImage(image, locationX, locationY);
-            };
-          }
-        });
-      }
-    };
-
-    renderCanvas(canvasLeftRef, page);
-    renderCanvas(canvasRightRef, page + 1);
+    renderCanvas(albumMockDataList, canvasLeftRef, page);
+    renderCanvas(albumMockDataList, canvasRightRef, page + 1);
   }, [page]);
 
   const onClickPrev = () => {
@@ -57,14 +37,16 @@ const AlbumSection = () => {
     <section>
       <div className="w-[1200px] flex">
         <button
-          className={`${page !== 1 && "hover:cursor-pointer"}`}
+          className={`${page !== 1 && "hover:cursor-pointer"} ${
+            page === 1 && "text-slate-300"
+          }`}
           disabled={page === 1}
           onClick={onClickPrev}
         >
           이전
         </button>
         <input className="border-2 block m-auto" />
-        {page === albumMockDataList.length - 1 ? (
+        {page >= albumMockDataList.length - 1 ? (
           <button className="hover:cursor-pointer" onClick={onClickNext}>
             생성
           </button>
---
+++ src/templates/album/lib/utils/index.ts
@@ -0,0 +1,25 @@
+export const renderCanvas = (
+  dataList: any,
+  canvasRef: any,
+  currentPage: number
+) => {
+  const canvas = canvasRef.current;
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  console.log(currentPage);
+  const currentPageData = dataList.find(
+    (data: any) => data.page === currentPage
+  );
+
+  if (currentPageData) {
+    currentPageData.imgList.forEach(({ src, locationX, locationY }: any) => {
+      if (src) {
+        const image = new Image();
+        image.src = src;
+        image.onload = () => {
+          ctx.drawImage(image, locationX, locationY);
+        };
+      }
+    });
+  }
+};
