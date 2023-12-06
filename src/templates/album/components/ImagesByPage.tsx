ef9c8ffc47998aca7a7ab2581fcfd7b5cbf6fd01
rlarlduf20 <kky8274@naver.com>
2023-12-06 21:00:37 +0900

    Feat: 이미지 로테이션 및 리사이징 범위 제한 추가

--- src/templates/album/components/ImagesByPage.tsx
+++ src/templates/album/components/ImagesByPage.tsx
@@ -22,6 +22,46 @@ interface ImagePropType {
   onChange: (e: any) => void;
 }
 
+const getCorner = (
+  pivotX: number,
+  pivotY: number,
+  diffX: number,
+  diffY: number,
+  angle: number
+) => {
+  const distance = Math.sqrt(diffX * diffX + diffY * diffY);
+
+  /// find angle from pivot to corner
+  angle += Math.atan2(diffY, diffX);
+
+  /// get new x and y and round it off to integer
+  const x = pivotX + distance * Math.cos(angle);
+  const y = pivotY + distance * Math.sin(angle);
+
+  return { x: x, y: y };
+};
+const getImage = (rotatedImg: any) => {
+  const { x, y, width, height } = rotatedImg;
+  const rad = rotatedImg.rotation;
+
+  const p1 = getCorner(x, y, 0, 0, rad);
+  const p2 = getCorner(x, y, width, 0, rad);
+  const p3 = getCorner(x, y, width, height, rad);
+  const p4 = getCorner(x, y, 0, height, rad);
+
+  const minX = Math.min(p1.x, p2.x, p3.x, p4.x);
+  const minY = Math.min(p1.y, p2.y, p3.y, p4.y);
+  const maxX = Math.max(p1.x, p2.x, p3.x, p4.x);
+  const maxY = Math.max(p1.y, p2.y, p3.y, p4.y);
+
+  return {
+    x: minX,
+    y: minY,
+    width: maxX - minX,
+    height: maxY - minY,
+  };
+};
+
 const ImagesByPage = ({
   bodyData,
   imageInfo,
@@ -111,8 +151,23 @@ const ImagesByPage = ({
         <Transformer
           ref={trRef}
           flipEnabled={false}
+          // boundBoxFunc={(oldBox, newBox) => {
+          //   if (Math.abs(newBox.width) < 5 || Math.abs(newBox.height) < 5) {
+          //     return oldBox;
+          //   }
+          //   return newBox;
+          // }}
           boundBoxFunc={(oldBox, newBox) => {
-            if (Math.abs(newBox.width) < 5 || Math.abs(newBox.height) < 5) {
+            const box = getImage(newBox);
+            const isOut =
+              box.x < 0 ||
+              box.y < 0 ||
+              box.x + box.width > 1200 ||
+              box.y + box.height > 800;
+
+            // if new bounding box is out of visible viewport, let's just skip transforming
+            // this logic can be improved by still allow some transforming if we have small available space
+            if (isOut) {
               return oldBox;
             }
             return newBox;
