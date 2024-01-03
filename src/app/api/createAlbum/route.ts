4881a9f73345d91e951a885758f2e433a3e7455b
rlarlduf20 <kky8274@naver.com>
2024-01-03 17:41:16 +0900

    Feat: 앨범 생성 요청 보내기

--- src/app/api/createAlbum/route.ts
+++ src/app/api/createAlbum/route.ts
@@ -2,16 +2,19 @@ import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/route";
 
-export async function POST() {
+export async function POST(request: Request) {
   const { jogakTokens } = await getServerSession(authOptions);
+  const data = await request.json();
 
-  const res = await fetch(`${process.env.SERVER_URL}/logout`, {
+  const res = await fetch(`${process.env.SERVER_URL}/album`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${jogakTokens.accessToken}`,
     },
+    body: JSON.stringify({
+      title: data.title,
+    }),
   });
-
   return NextResponse.json(res);
 }
--- src/templates/AddAlbumSection/AddButton.tsx
+++ src/templates/AddAlbumSection/AddButton.tsx
@@ -9,9 +9,20 @@ const AddButton = ({ title, handleInputTitle }: AddButtonPropsType) => {
       alert("제목을 입력해주세요.");
       return;
     }
+    const res = await fetch("api/createAlbum", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+      }),
+    });
+    if (!res.ok) {
+      alert("다시 시도해주세요!");
+      return;
+    }
+    console.log(res);
     handleInputTitle("");
-    console.log(title);
   };
+
   return <button onClick={handleClick}>만들기</button>;
 };
 
