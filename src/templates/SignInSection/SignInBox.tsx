d12ec1d78dd0fae3bf51a57c07b1e6c98b43eef9
rlarlduf20 <kky8274@naver.com>
2024-01-23 21:57:59 +0900

    Feat: 로그인 로딩 UI 추가

--- src/templates/SignInSection/SignInBox.tsx
+++ src/templates/SignInSection/SignInBox.tsx
@@ -1,27 +1,42 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import KakaoLoginBtn from "../../../public/images/sign/kakao_login_btn.png";
 import NaverLoginBtn from "../../../public/images/sign/naver_login_btn.png";
 import { signIn } from "next-auth/react";
+import LoadingGIF from "../../../public/images/loading_gif.png";
 
 const SignInBox = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div>
+      {isLoading && (
+        <div className="fixed z-30 flex justify-center items-center top-0 left-0 w-full h-full bg-main_black bg-opacity-50">
+          <Image src={LoadingGIF} alt="로딩" width={80} height={80} />
+        </div>
+      )}
       <Image
         src={KakaoLoginBtn}
         alt="카카오 로그인"
         className="mb-[15px] cursor-pointer"
-        width={330}
-        onClick={() => signIn("kakao")}
+        width={300}
+        onClick={async () => {
+          signIn("kakao");
+          setIsLoading((prev) => !prev);
+        }}
       />
 
       <Image
         src={NaverLoginBtn}
         alt="네이버 로그인"
         className="cursor-pointer"
-        width={330}
-        onClick={() => signIn("naver")}
+        width={300}
+        onClick={async () => {
+          signIn("naver");
+          setIsLoading((prev) => !prev);
+        }}
       />
     </div>
   );
