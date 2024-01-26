f95c50e196018147efca55e28a19bab01ec35d6e
rlarlduf20 <kky8274@naver.com>
2024-01-26 15:26:33 +0900

    Feat: 친구리스트에 우클릭 이벤트 추가
    우클릭 시 각각의 친구에 대해 삭제하거나 짱친등록이 가능한 박스 UI 생성

--- src/templates/MainSection/MateList.tsx
+++ src/templates/MainSection/MateList.tsx
@@ -1,13 +1,34 @@
 "use client";
 
+import { useState, useRef } from "react";
 import Trapezoid from "@/components/Trapezoid";
 import { mockFriendsList } from "@/assets/mockData";
+import useMouseDownOutside from "@/hooks/useMouseDownOutside";
 
 const MateList = () => {
+  const [openIdx, setOpenIdx] = useState<number>(-1);
+  const editBoxRef = useRef<HTMLDivElement>(null);
+  const { isOpen, setIsOpen } = useMouseDownOutside(editBoxRef);
+
+  const handleRightClick = (e: React.MouseEvent, index: number) => {
+    e.preventDefault();
+    setIsOpen(true);
+    setOpenIdx(index);
+  };
   return (
-    <div className="h-[250px] overflow-scroll">
+    <div ref={editBoxRef} className="h-[250px] overflow-scroll">
       {mockFriendsList.map((item, index) => (
-        <div key={index} className="flex items-center mb-[20px]">
+        <div
+          key={index}
+          className="relative flex items-center mb-[20px]"
+          onContextMenu={(e) => handleRightClick(e, index)}
+        >
+          {isOpen && openIdx === index && (
+            <div className="absolute top-[15px] right-[0px] w-[80px] h-[56px] bg-main_black border-white border-[1px] px-[12px] py-[5px]">
+              <p className="text-[14px] mb-[4px] cursor-pointer">친구 삭제</p>
+              <p className="text-[14px] cursor-pointer">짱친 등록</p>
+            </div>
+          )}
           <Trapezoid
             styles={{
               width: "40px",
