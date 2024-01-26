f306e1d93bba2999369f09d5957b92bc59ee7d55
rlarlduf20 <kky8274@naver.com>
2024-01-26 16:41:15 +0900

    Feat: 로그인 에러 페이지 Ui 수정

--- src/app/(landing)/signIn/error/page.tsx
+++ src/app/(landing)/signIn/error/page.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+import ErrorLogo from "../../../../../public/images/error.png";
 import SignButton from "@/components/SignButton";
 
 const SignError = () => {
   return (
-    <section>
-      <p>오류가 발생했습니다. 다시 로그인 해주세요.</p>
+    <section className="w-full h-[100vh] flex flex-col justify-center items-center gap-[35px] mt-[-80px]">
+      <Image src={ErrorLogo} alt="에러" />
+      <p className="text-[20px]">로그인에 실패했습니다. 다시 시도해주세요.</p>
       <SignButton />
     </section>
   );
