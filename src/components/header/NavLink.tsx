fda10c0f47ac7280052efc4aa70eb76455ed6d72
rlarlduf20 <kky8274@naver.com>
2024-02-18 21:34:16 +0900

    Feat: 둘러보기 링크 아이콘 변경

--- src/components/header/NavLink.tsx
+++ src/components/header/NavLink.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import RouteTrapezoidIcon from "../../../public/images/svg/route-trapezoid.svg";
+import BrowseIcon from "../../../public/images/svg/browse.svg";
 
 const NavLink = () => {
   const pathname = usePathname();
@@ -18,8 +19,9 @@ const NavLink = () => {
       ) : (
         pathname !== "/my/profile" && (
           <>
-            <Image src={RouteTrapezoidIcon} alt="사다리꼴 아이콘" />
-            <Link href="/browse">둘러보기</Link>
+            <Link href="/browse">
+              <Image src={BrowseIcon} alt="둘러보기 아이콘" />
+            </Link>
           </>
         )
       )}
