cd915b3e9afc037b1d683ddaa7a2a447cd9d744a
rlarlduf20 <kky8274@naver.com>
2024-02-18 21:25:35 +0900

    Feat: 둘러보기 페이지 텍스트 변경

--- src/templates/BrowseSection/index.tsx
+++ src/templates/BrowseSection/index.tsx
@@ -10,9 +10,7 @@ const NoFriend = () => {
     <section className="flex flex-col h-[calc(100vh-80px)] justify-center items-center">
       <Image src={ExclamationMarkIcon} alt="느낌표 아이콘" />
       <p className="text-[20px] mt-[22px] mb-[51px] text-center">
-        앗 둘러볼 친구가 없어요!
-        <br />
-        친구를 추가해주세요.
+        둘러보기는 준비중이에요!
       </p>
       <div className="flex gap-[3px]">
         <Image src={RouteTrapezoidIcon} alt="사다리꼴 아이콘" />
