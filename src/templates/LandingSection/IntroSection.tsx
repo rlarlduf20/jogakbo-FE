62c67b51d4b773f8b980017df534d6009bcaef07
rlarlduf20 <kky8274@naver.com>
2024-02-21 15:57:14 +0900

    Feat: 인트로 텍스트 추가

--- src/templates/LandingSection/IntroSection.tsx
+++ src/templates/LandingSection/IntroSection.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import LandingTitleGIF from "../../../public/images/jogak-landing-title.gif";
+import IntroText from "./IntroText";
 
 const IntroSection = () => {
   return (
-    <section className="w-full mx-auto">
+    <section className="mt-[-80px]">
       <Image src={LandingTitleGIF} width={1200} alt="gif" />
+      <IntroText />
     </section>
   );
 };
---
+++ src/templates/LandingSection/IntroText.tsx
@@ -0,0 +1,5 @@
+const IntroText = () => {
+  return <p>언제 어디서나 흩어진 사진들을 모아볼 수 있으면 어떨까?</p>;
+};
+
+export default IntroText;
