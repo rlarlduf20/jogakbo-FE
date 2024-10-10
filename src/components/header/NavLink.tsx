fee53491f2715f99f657f07a46ddb04e790d43b4
rlarlduf20 <kky8274@naver.com>
2024-10-10 16:17:32 +0900

    Fix: 빌드 에러 수정

--- src/components/header/NavLink.tsx
+++ src/components/header/NavLink.tsx
@@ -3,44 +3,42 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import MyJogakboIcon from "../../../public/images/svg/my-jogakbo.svg";
+
 import BrowseIcon from "../../../public/images/svg/browse.svg";
-import useHoverText from "@/hooks/useHoverText";
+import MyJogakboIcon from "../../../public/images/svg/my-jogakbo.svg";
 import HoverText from "../HoverText";
+
+import useHoverText from "@/hooks/useHoverText";
 
 const NavLink = () => {
   const pathname = usePathname();
   const { isHoverIcon, handleIsHoverToTrue, handleIsHoverToFalse } =
     useHoverText();
 
-  return (
-    <>
-      {pathname === "/browse" ? (
-        <div className="relative whitespace-nowrap">
-          <Link
-            href="/"
-            onMouseOver={handleIsHoverToTrue}
-            onMouseLeave={handleIsHoverToFalse}
-          >
-            <Image src={MyJogakboIcon} alt="내 조각보 아이콘" />
-          </Link>
-          {isHoverIcon && <HoverText>내 조각보</HoverText>}
-        </div>
-      ) : (
-        pathname !== "/my/profile" && (
-          <div className="relative whitespace-nowrap">
-            <Link
-              href="/browse"
-              onMouseOver={handleIsHoverToTrue}
-              onMouseLeave={handleIsHoverToFalse}
-            >
-              <Image src={BrowseIcon} alt="둘러보기 아이콘" />
-            </Link>
-            {isHoverIcon && <HoverText>둘러보기</HoverText>}
-          </div>
-        )
-      )}
-    </>
+  return pathname === "/browse" ? (
+    <div className="relative whitespace-nowrap">
+      <Link
+        href="/"
+        onMouseOver={handleIsHoverToTrue}
+        onMouseLeave={handleIsHoverToFalse}
+      >
+        <Image src={MyJogakboIcon} alt="내 조각보 아이콘" />
+      </Link>
+      {isHoverIcon && <HoverText>내 조각보</HoverText>}
+    </div>
+  ) : (
+    pathname !== "/my/profile" && (
+      <div className="relative whitespace-nowrap">
+        <Link
+          href="/browse"
+          onMouseOver={handleIsHoverToTrue}
+          onMouseLeave={handleIsHoverToFalse}
+        >
+          <Image src={BrowseIcon} alt="둘러보기 아이콘" />
+        </Link>
+        {isHoverIcon && <HoverText>둘러보기</HoverText>}
+      </div>
+    )
   );
 };
 
